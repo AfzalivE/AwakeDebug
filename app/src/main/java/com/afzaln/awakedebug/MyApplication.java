c73530cf614c266f8b6762dd56876419e1201c40
Afzal Najam <afzalnaj@gmail.com>
2018-09-18 22:05:46 -0400

    Remove crashlytics because Google took down the app for violation

--- app/src/main/java/com/afzaln/awakedebug/MyApplication.java
+++ app/src/main/java/com/afzaln/awakedebug/MyApplication.java
@@ -2,8 +2,6 @@ package com.afzaln.awakedebug;
 
 import android.app.Application;
 
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 import timber.log.Timber.DebugTree;
 
@@ -15,9 +13,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (!BuildConfig.DEBUG) {
-            Fabric.with(this, new Crashlytics());
-        } else {
+        if (BuildConfig.DEBUG) {
             Timber.plant(new DebugTree());
         }
     }
